032df11356a783f40614992ecf3bcf0d3d44b562
zeddling <victorzeddys@gmail.com>
2021-11-11 18:03:08 +0300

    Added table

--- index.js
+++ index.js
@@ -1,50 +1,53 @@
-var server = new StellarSdk.Server("https://horizon.stellar.org");
+$(document).ready(() => {
+    console.log('ready')
+    var server = new StellarSdk.Server("https://horizon.stellar.org");
+    var data_array = [];
+    var tbody = document.getElementById("transactions");
 
-var ul = document.getElementById("transactions");
+    $("#fetchButton").on("click", () => {
+        console.log("clicked");
+        data_array = []; // clear array
+        server.transactions()
+            .call()
+            .then(function (page) {
+                $("#transactions").empty()
+                console.log('Page 1: ');
+                console.log(page.records);
 
-var handleFetchTransactions = () => {
-    server.transactions()
-    .call()
-    .then(function (page) {
-        console.log('Page 1: ');
-        console.log(page.records);
-        page.records.forEach(record => {
-            var div = document.createElement("div");
-            div.classList.add("col-sm-2");
+                var i = 1;  //  rows iterator
+                var j = 0;  //  array iterator
+                page.records.forEach(record => {
+                    var row = document.createElement("tr");
 
-            var li = document.createElement("li");
-            li.appendChild(createCard(record["id"]));
+                    var id = document.createElement("td");
+                    id.innerHTML = i;
+                    row.appendChild(id);
+                    data_array[j] = record["id"];
 
-            div.appendChild(li);
-            ul.appendChild(div);
-        });
-        return page.next();
-    })
-    .then(function (page) {
-        console.log('Page 2: ');
-        console.log(page.records);
-    })
-    .catch(function (err) {
-        console.log(err);
+                    var memo = document.createElement("td");
+                    memo.innerHTML = record["memo"];
+                    row.appendChild(memo);
+
+                    var fee_charged = document.createElement("td");
+                    fee_charged.innerHTML = record["fee_charged"];
+                    row.appendChild(fee_charged);
+
+                    var created_at = document.createElement("td");
+                    created_at.innerHTML = record["created_at"];
+                    row.appendChild(created_at);
+
+                    tbody.appendChild(row);
+                    i++;
+                });
+                return page.next();
+            })
+            .then(function (page) {
+                console.log('Page 2: ');
+                console.log(page.records);
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
     });
-}
 
-function createCard(id) {
-    //  Style card
-    var card = document.createElement("div");
-    card.classList.add("card");
-    card.style.width = "18rem";
-
-    //  Card body
-    var body = document.createElement("div");
-    body.classList.add("card-body");
-
-    //  Append card info
-    var title = document.createElement("h5");
-    title.classList.add("card-title");
-    title.innerText = id;
-
-    body.appendChild(title);
-    card.appendChild(body);
-    return card;
-}
+});
